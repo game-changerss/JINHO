294c24af867a5ad69bc5a3555acf0de3a5787c11
Jinho <jinho@jinhos-MacBook-Pro.local>
2023-02-08 16:10:19 +0900

    asd

--- Week01/BaekJoon_1620.java
+++ Week01/BaekJoon_1620.java
@@ -34,21 +34,21 @@ public class BaekJoon_1620 {
         int totalCnt = Integer.parseInt(st.nextToken());
         int questionCnt = Integer.parseInt(st.nextToken());
 
-        Map<Integer, String> ismap = new HashMap<>();
-        Map<String, Integer> simap = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
+        String[] nameList = new String[totalCnt];
         for(int i=0; i<totalCnt; i++){
             String collection = br.readLine();
-            ismap.put(i, collection);
-            simap.put(collection, i);
+            map.put(collection, i+1);
+            nameList[i] = collection;
         }
 
         for(int i=0; i<questionCnt; i++){
             String temp = br.readLine();
             try {
                 int idx = Integer.parseInt(temp);
-                bw.write(ismap.get(idx-1)+"\n");
+                bw.write(nameList[idx-1]+"\n");
             }catch (NumberFormatException nfe){
-                bw.write((simap.get(temp)+1)+"\n");
+                bw.write((map.get(temp))+"\n");
             }
         }
         bw.close();
